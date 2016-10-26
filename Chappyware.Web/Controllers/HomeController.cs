f4133daf4cb47b9697b27fb87a7c882e70ced41c
David <david.dchapman@gmail.com>
2016-10-25 20:22:46 -0700

    Added the teams route to retieve the teams as a json blob

--- Chappyware.Web/Controllers/HomeController.cs
+++ Chappyware.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Chappyware.Business;
 using Chappyware.Data;
+using Chappyware.Data.Storage;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,41 @@ namespace Chappyware.Web.Controllers
 {
     public class HomeController : Controller
     {
+
+        private const string _GetTeamRoute = "~/teams";
+        private const string _UpdateTeamRoute = "~/teams/update";
+
         // GET: Home
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route(_GetTeamRoute, Name ="teams")]
+        public ActionResult GetTeams()
         {
             FantasyTeamManager manager = new FantasyTeamManager();
             FantasyLeague league = manager.CreateLeague("Robs");
             manager.UpdateLeagueRoster(league, "TeamImport.txt");
+            JsonResult result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            result.Data = JsonConvert.SerializeObject(league.Teams);
+            return result;
+        }
+
+        [Route(_UpdateTeamRoute)]
+        public ActionResult UpdateTeams()
+        {
+            // get the current stats and load them into memory
+            IStatSource csvSource = new HockeyReferenceDotComStatSource();
+            csvSource.Initialize();
+
+            List<Player> currentPlayerStats = StorageFactory.Instance.LoadPersistedStatSource();
+            StatisticManager.UpdatePlayerStatistics(currentPlayerStats, csvSource);
+
+            // persist them into json
+            StorageFactory.Instance.UpdatedPersistedStatSource(currentPlayerStats);
+
             return View();
         }
     }
